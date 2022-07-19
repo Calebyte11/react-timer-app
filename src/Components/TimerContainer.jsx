17fef6db6d7ca5c7667da9b503335be082b2243d
Calebyte11 <calebytedembelle11@gmail.com>
2022-07-19 14:17:02 +0100

    introducing and using 'useEffect Hooks' to cause side effects(timing - setInterval function) in TimerContainer.jsx

--- src/Components/TimerContainer.jsx
+++ src/Components/TimerContainer.jsx
@@ -2,32 +2,42 @@ import React from 'react';
 import TimerForm from './TimerForm';
 import TimerMain from './TimerMain';
 // import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function TimerContainer(props) {
-  
-    const [formopen, setformopen] = useState({
-      isFormOpen : false
-    })
 
-    const [timingEls, settimingEls] = useState({
-      daysEl : '00',
-      hoursEl : '00',
-      minutesEl : '00',
-      secondsEl : '00'
-    })
-
-    
   const timers = props.timers;
+
+  // ............ THE STATES ...............
+
+  const [formopen, setformopen] = useState({
+    isFormOpen : false
+  })
+
+  const [timingEls, settimingEls] = useState({
+    daysEl : '00',
+    hoursEl : '00',
+    minutesEl : '00',
+    secondsEl : '00'
+  })
+
     
-  
+  // ======= THE SIDE EFFECTS (useEffect....).......
+  useEffect ( (props) => {
+    const interval = setInterval( () => countdownTimer(), 1000 );
+
+    return () => {
+      clearInterval(interval);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timingEls] )
   
   //=========== TimerContainer's click handlers (edit click and delete click) =============
   const editClickHandler = (e) => {
-      setformopen({
-          isFormOpen : true
-      })
+    setformopen({
+        isFormOpen : true
+    })
   };
 
   const deleteClickHandler = (e) => {
@@ -38,80 +48,68 @@ function TimerContainer(props) {
 
    // ==== functions handling  in the FORM (save and cancel buttons)======
   const saveClick = (updatetimer) => {
-      setformopen({
-          isFormOpen : false
-      })
-      console.log('SUBMIT');
-      props.onEditFormSubmit(updatetimer);
-      console.log(updatetimer);
+    setformopen({
+        isFormOpen : false
+    })
+    console.log('SUBMIT');
+    props.onEditFormSubmit(updatetimer);
+    console.log(updatetimer);
   };
 
   const cancelClick = (e) => {
-     setformopen({
-          isFormOpen : false
-      })
-     console.log('CANCEL');
+    setformopen({
+        isFormOpen : false
+    })
+    console.log('CANCEL');
   };
 
 
 
    // ============ MAIN TIMING FUNCTION =========
-  const countdownTimer = (timers) => {
-        
+  const countdownTimer = () => {
+    
+    const timers = props.timers
     const stoppageDate = new Date(timers.dateTime);
     
     if(stoppageDate ===  'null'){
-        console.log("Invalid Date !!! Please Enter Correct Date!!");
-    }
-
-    //========== TIMER IN INTERVALS ================
-    setInterval(() => {
-        
-        const currentDate = new Date();
-        
-        const totalSeconds = (stoppageDate - currentDate) / 1000;
-        
-        
-        renderTimer(totalSeconds)
-    }, 1000);
-
-    function renderTimer (totalSeconds) {
-
-      if (totalSeconds < 0) {
-          
-          console.log('Invalid Date !!! Please Enter Correct Date!!');
-                  
-
-      } else if(totalSeconds > 0) {
-              
-          const days = Math.floor(totalSeconds / 3600 / 24 );
-          const hours = Math.floor(totalSeconds / 3600) % 24;
-          const minutes = Math.floor(totalSeconds / 60) % 60;
-          const seconds = Math.floor(totalSeconds) %60;
-
-          const daysEls = formatTime(days);
-          const hoursEls = formatTime(hours);
-          const minutesEls = formatTime(minutes);
-          const secondsEls = formatTime(seconds);
-          
-          // console.log(daysEls, hoursEls, minutesEls, secondsEls);
-          settimingEls({
-              daysEl : daysEls,
-              hoursEl : hoursEls,
-              minutesEl : minutesEls,
-              secondsEl : secondsEls
-          })
-
-      };
-        
+        alert("Invalid Date !!! Please Enter Correct Date!!");
     };
 
+    const currentDate = new Date();
+    
+    const totalSeconds = (stoppageDate - currentDate) / 1000;
+        
+    if (totalSeconds < 0) {
+        
+        console.log('Invalid Date !!! Please Enter Correct Date!!');
+                
+
+    } else if(totalSeconds > 0) {
+            
+      const days = Math.floor(totalSeconds / 3600 / 24 );
+      const hours = Math.floor(totalSeconds / 3600) % 24;
+      const minutes = Math.floor(totalSeconds / 60) % 60;
+      const seconds = Math.floor(totalSeconds) %60;
+
+      const daysEls = formatTime(days);
+      const hoursEls = formatTime(hours);
+      const minutesEls = formatTime(minutes);
+      const secondsEls = formatTime(seconds);
+      
+      settimingEls({
+        daysEl : daysEls,
+        hoursEl : hoursEls,
+        minutesEl : minutesEls,
+        secondsEl : secondsEls
+      });
+
+    };
+      
   };
+
   const formatTime = (time) => {
     return time < 10 ? `0${time}` : time;
   };
-  countdownTimer(timers);
-
 
 
   //======================================= RENDERING =================================================================
@@ -125,6 +123,6 @@ function TimerContainer(props) {
   }
 
 
-}
+};
 
 export default TimerContainer
